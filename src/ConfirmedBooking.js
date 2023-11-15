209c5ff6830f2f5a6581ed486a3ceefb7d4dd349
Rey <ikwuezumaeloray@gmail.com>
2023-11-15 22:15:36 +0100

    working on the booking form

--- src/ConfirmedBooking.js
+++ src/ConfirmedBooking.js
@@ -1,15 +1,18 @@
-'./ConfirmedBooking.css';
+import './ConfirmedBooking.css';
 
 export default function ConfirmedBooking() {
     return (
         <>
-        <main className=" confirmed">
+        <main className="confirmed">
+        <div className='backdrop'>
         <img src= "/Yellowreserve.svg" width={250} height={250} alt=" a reservation card " className="reserve"></img>
-            <h2 className=" confirmed-heading"> Thank you! </h2>
-            <p className=" confirmed-paragraph"> your reservation was successful. </p>
-            <p className=" confirmed-paragraph"> Please provide your name and number, or the QR code sent to your email at the reception upon arrival. </p>
-            <p className=" confirmed-paragraph"> We look forward to serve you! </p>
-        
+            <h2 className="confirmed-heading"> Thank you! </h2>
+            <p className="confirmed-paragraph"> Your reservation was successful!</p>
+            <p className="confirmed-paragraph1"> A confirmation email was sent to your email address. </p>
+            <p className="confirmed-paragraph2"> Please provide your name and number,  </p>
+            <p className="confirmed-paragraph3"> or the QR code sent to your email at the reception upon your arrival. </p>
+            <p className="confirmed-paragraph4"> We look forward to serving you! </p>
+            </div>
         </main>
         </>
     )
